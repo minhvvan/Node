b3873ada2c9ff60f76c59459a7d9c8893b8201fa
unknown <eminhwan12@gmail.com>
2020-12-29 18:52:36 +0900

    post register

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
 const mongoose = require('mongoose')
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+//application/json
+app.use(bodyParser.json())
+
 mongoose.connect('mongodb+srv://minhwan:asd1234@first.li0k4.mongodb.net/First?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -12,6 +18,20 @@ mongoose.connect('mongodb+srv://minhwan:asd1234@first.li0k4.mongodb.net/First?re
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    //회원 가입 관련 정보 clinet --> db
+
+    const user = new User(req.body)
+    //user모델에 저장
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
