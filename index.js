415379324a45cffb9c52cc2abfc45dff86e92199
unknown <eminhwan12@gmail.com>
2021-01-04 01:06:58 +0900

    hash password

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser')
 const { User } = require("./models/User")
 const mongoose = require('mongoose')
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 //application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 const config = require('./config/key')
 
 mongoose.connect(config.mongoURI,{
@@ -22,8 +22,9 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/register', (req, res) => {
     //회원 가입 관련 정보 clinet --> db
-
     const user = new User(req.body)
+    console.log(req.body)
+
     //user모델에 저장
     user.save((err, userInfo) => {
         if(err) return res.json({success: false, err})
--- models/User.js
+++ models/User.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
         type: String,
         maxlength: 50
     },
-    email:{
+    email: {
         type: String,
         trim: true,
         unique: 1
     },
-    password:{
+    password: {
         type: String,
         minlength: 5
     },
-    lastname:{
+    lastname: {
         type: String,
         maxlength: 50
     },
-    role:{
+    role: {
         type: Number,
         default: 0
     },
@@ -28,6 +30,25 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+    //비밀번호 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+
+                bcrypt.hash(user.password, salt, function(err, hash){
+                    if(err) return next(err)
+                    user.password = hash
+                    next()
+                })
+            });
+        });
     }
 })
 
