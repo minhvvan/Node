67710725c744e353e8f327faa9b3a76dd1f2fb20
minhvvan <eminhwan12@gmail.com>
2021-01-06 19:51:51 +0900

    Create login router

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const port = 5000
 const bodyParser = require('body-parser')
 const { User } = require("./models/User")
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser())
 const config = require('./config/key')
 
 mongoose.connect(config.mongoURI,{
@@ -35,5 +37,32 @@ app.post('/register', (req, res) => {
 
 })
 
+app.post('/login', (req, res) => {
+    //요청된 이메일을 DB에서 찾는다
+    User.findOne({ email: req.body.email }, (err, user)=> {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "email이 일치하는 유저가 없습니다."
+            })
+        }
+    
+        //비밀번호 일치 여부 확인
+        user.comparePassword(req.body.password, (err, isMatch) =>{
+            if(!isMatch)
+                return res.json({loginSuccess: false, message: "비밀번호 불일치"})
+
+            //Token생성
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err)
+                
+                //토큰을 저장(쿠기, 로컬, etc)
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id })
+            })
+        })
+    })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -49,8 +50,29 @@ userSchema.pre('save', function(next){
                 })
             });
         });
+    }else{
+        next()
     }
 })
+
+//비밀번호 비교
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+            cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    //jsonwebtoken을 이용하여 token 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
